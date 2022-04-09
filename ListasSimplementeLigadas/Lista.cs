53d945b56797efbc731970d55fa7412d28d66ac4
Octavio Garcia <jss670@gmail.com>
2022-04-09 14:50:54 -0700

    Agregar archivos de proyecto.

---
+++ ListasSimplementeLigadas/Lista.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasSimplementeLigadas
+{
+    public class Lista
+    {
+        Nodo nodoInicial;
+        Nodo nodoActual;
+        public Lista()
+        {
+            nodoInicial = new Nodo();
+        }
+        public bool ValidaVacio()
+        {
+            return (nodoInicial.Enlace == null);
+        }
+        public void Vaciar()
+        {
+            nodoInicial.Enlace = null;
+        }
+        public string RecorrerLista()
+        {
+            string datoslista = "";
+            nodoActual = nodoInicial;
+            while (nodoActual.Enlace != null)
+            {
+                nodoActual = nodoActual.Enlace;
+                datoslista += $"{nodoActual.Valor}\n";
+            }
+            return datoslista;
+        }
+        public void AgregarNodo(string dato)
+        {
+            nodoActual = nodoInicial;
+            while (nodoActual.Enlace != null)
+            {
+                nodoActual = nodoActual.Enlace;
+            }
+            Nodo nodoNuevo = new Nodo(dato);
+            nodoActual.Enlace = nodoNuevo;
+        }
+        public Nodo Buscar(string dato)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.Enlace != null)
+                {
+                    nodoBusqueda = nodoBusqueda.Enlace;
+                    if (nodoBusqueda.Valor == dato)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+        public Nodo BuscarPorIndice(int indice)
+
+
+        {
+            int Indice = -1;
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.Enlace != null)
+                {
+                    nodoBusqueda = nodoBusqueda.Enlace;
+                    indice++;
+                    if (Indice == indice)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public Nodo BuscarAnterior(string dato)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.Enlace != null && nodoBusqueda.Enlace.Valor != dato)
+                {
+                    nodoBusqueda = nodoBusqueda.Enlace;
+                    if (nodoBusqueda.Enlace.Valor == dato)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+        public void BorrarNodo(string dato)
+        {
+            if (ValidaVacio() == false)
+            {
+                nodoActual = Buscar(dato);
+                if (nodoActual != null)
+                {
+                    Nodo nodoAnterior = BuscarAnterior(dato);
+                    nodoAnterior.Enlace = nodoActual.Enlace;
+                    nodoActual.Enlace = null;
+
+                }
+            }
+        }
+    }
+}
---
+++ ListasSimplementeLigadas/Program.cs
@@ -0,0 +1,13 @@
+﻿using ListasSimplementeLigadas;
+
+Lista listas = new Lista();
+listas.AgregarNodo("uno");
+listas.AgregarNodo("dos");
+listas.AgregarNodo("tres");
+Console.WriteLine(listas.RecorrerLista());
+Console.WriteLine("buscando nodo dos");
+Nodo nodoDos = listas.Buscar("dos");
+Console.WriteLine(nodoDos.Valor);
+Console.WriteLine("borrando dos");
+listas.BorrarNodo("dos");
+Console.WriteLine(listas.RecorrerLista());
